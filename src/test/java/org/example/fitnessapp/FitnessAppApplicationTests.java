52a56305381e03f10b13ab202beaf127198a05c0
RamilMusazada <themusazadeh@gmail.com>
2025-04-16 18:57:52 +0400

    Initial commit

---
+++ src/test/java/org/example/fitnessapp/FitnessAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.fitnessapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
